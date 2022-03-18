cf8519ca23ff37d0ba333ec4567d0646468283dc
Sols7 <serg-lex@yandex.ru>
2022-03-18 19:42:08 +0300

    spring JPA 2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,11 @@
-insert into films(title) values ('Harry Potter');
-insert into films(title) values ('Back to the future');
+INSERT INTO GENRE(ID, NAME) VALUES (1, 'fantasy');
+INSERT INTO GENRE(ID, NAME) VALUES (2, 'fantastic');
+
+insert into FILMS(ID, TITLE, GENRE_ID) values (1, 'Harry Potter', 1);
+insert into FILMS(ID, TITLE, GENRE_ID) values (2, 'Back to the future', 2);
+
+
+INSERT INTO PLACE(ID, NAME, FILM_ID) VALUES (1, 'London square', 1);
+INSERT INTO PLACE(ID, NAME, FILM_ID) VALUES (2, 'Train', 1);
+
+INSERT INTO PLACE(ID, NAME, FILM_ID) VALUES (3, 'USA', 2);
