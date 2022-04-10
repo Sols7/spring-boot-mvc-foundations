8f6e9458c04eaab650d180042b6a90a782713785
Sols7 <serg-lex@yandex.ru>
2022-04-10 17:43:00 +0300

    spring MVC 5

--- src/main/java/ru/itsjava/service/GenreService.java
+++ src/main/java/ru/itsjava/service/GenreService.java
@@ -1,6 +1,11 @@
 package ru.itsjava.service;
 
+import ru.itsjava.domain.Genre;
+
 public interface GenreService {
     void changeGenre(String oldName, String updatedName);
     void printGenre(String name);
+    void createGenre(Genre genre);
+    Genre getGenreById(long id);
+    void deleteGenreById(long id);
 }
