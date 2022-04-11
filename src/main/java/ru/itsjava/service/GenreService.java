19263b9fe6ae278861b2ecf7b8c19c2bcc8e11d3
Sols7 <serg-lex@yandex.ru>
2022-04-11 22:15:57 +0300

    spring MVC 6 - CRUD

--- src/main/java/ru/itsjava/service/GenreService.java
+++ src/main/java/ru/itsjava/service/GenreService.java
@@ -2,10 +2,14 @@ package ru.itsjava.service;
 
 import ru.itsjava.domain.Genre;
 
+import java.util.List;
+
 public interface GenreService {
     void changeGenre(String oldName, String updatedName);
     void printGenre(String name);
     void createGenre(Genre genre);
     Genre getGenreById(long id);
-    void deleteGenreById(long id);
+    void deleteGenre(Genre genre);
+    List<Genre> getAllGenres();
+    void updateGenre(Genre genre);
 }
